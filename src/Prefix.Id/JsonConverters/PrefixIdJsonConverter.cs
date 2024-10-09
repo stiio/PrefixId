828546d307005372d0d0928ac16a20a580041baf
Stio <proflolstik98@mail.ru>
2024-10-09 23:33:55 +0300

    rename projects

---
+++ src/Prefix.Id/JsonConverters/PrefixIdJsonConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Stio.Prefix.Id.Models;
+
+namespace Stio.Prefix.Id.JsonConverters;
+
+internal class PrefixIdJsonConverter<TId> : JsonConverter<TId> where TId : PrefixId, new()
+{
+    public override TId? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        if (reader.TokenType is JsonTokenType.Null)
+        {
+            return null;
+        }
+
+        var value = JsonSerializer.Deserialize<string>(ref reader, options);
+        var id = new TId { Value = value! };
+        return id;
+    }
+
+    public override void Write(Utf8JsonWriter writer, TId? value, JsonSerializerOptions options)
+    {
+        if (value is null)
+        {
+            writer.WriteNullValue();
+        }
+        else
+        {
+            JsonSerializer.Serialize(writer, value.Value, options);
+        }
+    }
+}
---
+++ src/Prefix.Id/Models/PrefixId.cs
@@ -0,0 +1,29 @@
+﻿namespace Stio.Prefix.Id.Models;
+
+public abstract record PrefixId
+{
+    private const string Separator = "_";
+    private readonly string value;
+
+    protected PrefixId()
+    {
+        // ReSharper disable once VirtualMemberCallInConstructor
+        this.value = this.Prefix + Separator + Ulid.NewUlid().ToString().ToLower();
+    }
+
+    public string Value
+    {
+        get => this.value;
+        init
+        {
+            if (value.Split(Separator).FirstOrDefault() != this.Prefix)
+            {
+                throw new Exception($"Id {value} has the wrong prefix. Expected: {this.Prefix}");
+            }
+
+            this.value = value;
+        }
+    }
+
+    protected abstract string Prefix { get; }
+}
